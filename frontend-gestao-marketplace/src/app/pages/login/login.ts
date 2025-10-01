41aecce4e636146abecb3004359a77d030e79cae
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-10-01 16:48:18 -0300

    chore: Implementação para realizar o login

--- frontend-gestao-marketplace/src/app/pages/login/login.ts
+++ frontend-gestao-marketplace/src/app/pages/login/login.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core'
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
+import { UserService } from '../../services/user'
+import { UserAuthService } from '../../services/user-auth'
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
+  loginErrorMessage = '';
 
+  userForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  private readonly _router = inject(Router);
+  private readonly _userService = inject(UserService);
+  private readonly _userAuthService = inject(UserAuthService);
+
+  login() {
+    if (this.userForm.invalid) return;
+
+    this._userService
+      .login(
+        this.userForm.get('email')?.value as string,
+        this.userForm.get('password')?.value as string
+      )
+      .subscribe({
+        next: (response) => {
+          this.loginErrorMessage = '';
+
+          // salvar o token no localstorage
+          this._userAuthService.setUserToken(response.data.token);
+
+          // redirecionar para tela de produtos
+          this._router.navigate(['/products']);
+        },
+        error: (error) => {
+          console.log(error);
+
+          this.loginErrorMessage = error.error.message;
+        },
+      });
+  }
 }
